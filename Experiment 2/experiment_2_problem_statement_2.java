1351c83d0eb34b145d40b15b8fce37d3291c699c
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-01-20 09:11:53 +0530

    Added Experiment-2 Solutions

---
+++ Experiment 2/experiment_2_problem_statement_2.java
@@ -0,0 +1,54 @@
+/*
+2.2	Write a program to implement selection sort algorithm.
+*/
+
+import java.util.Scanner;
+
+/**
+ * pracatical_2_problem_statement_2
+ */
+public class experiment_2_problem_statement_2 {
+
+    // Function to sort array using selection sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.print("Enter the Elements of Array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Unsorted Array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        selectionSort(arr);
+
+        System.out.print("Sorted Array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
